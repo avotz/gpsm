0c50f72d55b26810cdcc79aa8db487dbbe2d0177
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-08-10 00:35:28 -0600

    se agrego calendario cargando los schedules correspodientes

--- src/pages/medic-calendar/medic-calendar.ts
+++ src/pages/medic-calendar/medic-calendar.ts
@@ -1,82 +1,135 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-
+import { MedicServiceProvider } from '../../providers/medic-service/medic-service';
+import moment from 'moment'
 @Component({
   selector: 'page-medic-calendar',
   templateUrl: 'medic-calendar.html',
 })
 export class MedicCalendarPage {
    //@ViewChild(CalendarComponent) myCalendar:CalendarComponent;
-  calendarOptions : any;
-  options: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-     this.calendarOptions = this.navParams.data;
-      this.options = {
-        height: 'parent',
-        fixedWeekCount : false,
-        defaultDate: '2016-09-12',
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-        events: [
-          {
-            title: 'All Day Event',
-            start: '2016-09-01'
-          },
-          {
-            title: 'Long Event',
-            start: '2016-09-07',
-            end: '2016-09-10'
-          },
-          {
-            id: 999,
-            title: 'Repeating Event',
-            start: '2016-09-09T16:00:00'
-          },
-          {
-            id: 999,
-            title: 'Repeating Event',
-            start: '2016-09-16T16:00:00'
-          },
-          {
-            title: 'Conference',
-            start: '2016-09-11',
-            end: '2016-09-13'
-          },
-          {
-            title: 'Meeting',
-            start: '2016-09-12T10:30:00',
-            end: '2016-09-12T12:30:00'
-          },
-          {
-            title: 'Lunch',
-            start: '2016-09-12T12:00:00'
-          },
-          {
-            title: 'Meeting',
-            start: '2016-09-12T14:30:00'
-          },
-          {
-            title: 'Happy Hour',
-            start: '2016-09-12T17:30:00'
-          },
-          {
-            title: 'Dinner',
-            start: '2016-09-12T20:00:00'
-          },
-          {
-            title: 'Birthday Party',
-            start: '2016-09-13T07:00:00'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2016-09-28'
-          }
-        ]
-      };
+  params:any;
+  calendar : any;
+  eventSource: any[] = [];
+  schedules: any[] = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public medicService: MedicServiceProvider) {
+      this.params = this.navParams.data;
+      this.medicService = medicService;
+      this.calendar = {
+        currentDate:new Date(),
+        mode: 'month'
+      }
+
+     // this.loadEvents();
+     
+
     
   }
+  loadSchedules (date_from, date_to) {
+    this.medicService.findSchedules(this.params.medic_id, this.params.clinic_id, date_from, date_to)
+    .then(data => {
+        console.log(data)
+        //this.schedules = data.data;
+        data.forEach(schedule => {
+          console.log(schedule.start)
+          let intervals = this.createIntervalsFromHours(moment(schedule.start).format("YYYY-MM-DD"), moment(schedule.start).format("HH:mm"), moment(schedule.end).format("HH:mm"));
+
+          console.log(intervals);
+          let events = [];
+          for (var i = 0; i < intervals.length; i++) {
+            
+          
+            
+            let event = {
+                title: 'Disponible',
+                startTime: new Date(moment(schedule.start).format("YYYY-MM-DD") +' '+ intervals[i]),
+                endTime: new Date(moment(schedule.start).format("YYYY-MM-DD") +' '+ intervals[i+1]),
+                allDay: false
+            }
+
+            events.push(event);
+            
+          }
+
+          this.eventSource = events;
+          
+        
+        });
+        
+    })
+    .catch(error => alert(JSON.stringify(error)));
+
+  }
+  createIntervalsFromHours(date, from, until){
+     
+     until = Date.parse(date+" " + until);
+     from = Date.parse(date+" " + from);
+
+     let intervalLength = 30;
+     let intervalsPerHour = 60 / intervalLength;
+     let milisecsPerHour = 60 * 60 * 1000;
+    
+    var max = (Math.abs(until-from) / milisecsPerHour)* intervalsPerHour;
+           
+    var time = new Date(from);
+    var intervals = [];
+    for(var i = 0; i <= max; i++){
+        //doubleZeros just adds a zero in front of the value if it's smaller than 10.
+        var hour = this.doubleZeros(time.getHours());
+        var minute = this.doubleZeros(time.getMinutes());
+        intervals.push(hour+":"+minute);
+        time.setMinutes(time.getMinutes()+intervalLength);
+    }
+    return intervals;
+}
+
+
+doubleZeros(item){
+
+  return (item < 10 ) ? '0'+ item : item;
+}
+
+loadEvents() {
+      this.eventSource.push({
+          title: 'test',
+          startTime: new Date('2017-08-09 14:00:00'),
+          endTime: new Date('2017-08-09 14:30:00'),
+          allDay: false
+      });
+      this.eventSource.push({
+          title: 'test2',
+          startTime: new Date('2017-08-09 17:00:00'),
+          endTime: new Date('2017-08-09 17:30:00'),
+          allDay: false
+      });
+      //this.myCalendar.loadEvents();
+  }
+  onEventSelected (evt){
+    console.log(evt)
+  }
+  onViewTitleChanged (evt){
+
+  }
+  onTimeSelected (evt){
+   // console.log(evt.selectedTime)
+  }
+  onCurrentDateChanged(date){
+    //console.log('current date change - '+ evt)
+    //let currentDate = date;
+    //let firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    //let lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  
+    let dateFrom = moment(date).format('YYYY-MM-DD');
+    let dateTo = dateFrom; //moment(lastDay).format('YYYY-MM-DD');
+
+    console.log(dateFrom +' - '+ dateTo)
+     this.loadSchedules(dateFrom, dateTo);
+
+  }
+  onRangeChanged(evt){
+    console.log(evt);
+  }
+
   
 
   ionViewDidLoad() {
--- src/providers/medic-service/medic-service.ts
+++ src/providers/medic-service/medic-service.ts
@@ -31,5 +31,25 @@ export class MedicServiceProvider {
 
        
     }
+    findSchedules(medic_id, clinic_id,date_from,date_to) {
+
+      //url: '/medics/'+ externalEvent.data('doctor') +'/schedules/list?office='+ externalEvent.data('office'),
+
+         let params = {
+            office: clinic_id,
+            date1: date_from,
+            date2: date_to
+          }
+          let headers = new Headers({'Accept': 'application/json',
+                   'Authorization': 'Bearer '+ window.localStorage.getItem('token')}),
+
+              options = new RequestOptions({headers: headers,params: params});
+
+
+        return this.http.get(SERVER_URL + '/api/medics/'+ medic_id +'/schedules/list' , options)
+            .map(res => res.json())
+            .toPromise();
+
+    }
 
 }
